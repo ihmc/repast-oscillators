400b5382cf1ff9448f1ff0edd5685d4c8f023590
gbenincasa <gbenincasa@ihmc.us>
2017-10-25 17:26:07 -0400

    Added copyright notice.

--- src/us/ihmc/simon/joscillators/JOscillatorsBuilder.java
+++ src/us/ihmc/simon/joscillators/JOscillatorsBuilder.java
@@ -1,3 +1,23 @@
+/**
+ * This file is part of Repast Oscillators.
+ *
+ * Repast Oscillators is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Repast Oscillators is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Repast Oscillators. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Giacomo Benincasa	(gbenincasa@ihmc.us)
+ *
+ */
+
 package us.ihmc.simon.joscillators;
 
 import repast.simphony.context.Context;
@@ -18,11 +38,13 @@ public class JOscillatorsBuilder implements ContextBuilder<Oscillator> {
 	public Context<Oscillator> build(Context<Oscillator> context) {
 		context.setId("joscillators");
 
+		// Create space to graphically represent oscillators
 		OscillatorAdder adder = new OscillatorAdder(GUI.RADIOUS, GUI.PADDING);
 		ContinuousSpaceFactory spaceFactory =  ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
 		ContinuousSpace<Oscillator> space = spaceFactory.createContinuousSpace(
 				"space", context, adder, new StrictBorders(), adder.getSpaceWidth(), adder.getSpaceHeight());
 
+		// Added oscillators to context (and therefore to space, via OscillatorAdder)
 		for (int i = 0; i < Model.NUMBER_OF_OSCILLATORS; i++) {
 			context.add (new Kuramoto(OscillatorUtils.nextPhase(), OscillatorUtils.nextFrequency(), space));
 		}
