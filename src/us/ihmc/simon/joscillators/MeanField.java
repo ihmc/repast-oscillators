19217c170fb4db2ab21c3f9af8ba3819fda0d50a
gbenincasa <gbenincasa@ihmc.us>
2017-11-08 03:40:48 -0500

    CumulativePhase renamed into MeanField.

---
+++ src/us/ihmc/simon/joscillators/MeanField.java
@@ -0,0 +1,16 @@
+package us.ihmc.simon.joscillators;
+
+public class MeanField {
+	final double psi;
+	final double r;
+
+	/**
+	 * 
+	 * @param psi
+	 * @param r: phase coherence
+	 */
+	MeanField(double psi, double r) {
+		this.psi = psi;
+		this.r = r;
+	}
+}
